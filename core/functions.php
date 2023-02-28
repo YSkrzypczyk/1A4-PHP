a751d0eafe6a2f1f7fbf0525edfe287db2dea36d
skrzypczyk <ysk@wizards-technologies.com>
2023-02-28 12:54:18 +0100

    form register verif

--- core/functions.php
+++ core/functions.php
@@ -4,3 +4,16 @@ function helloWorld()
 {
 	echo "Hello world";
 }
+
+
+function cleanLastname($lastname){
+	return strtoupper(trim($lastname));
+}
+
+function cleanFirstname($firstname){
+	return ucwords(strtolower(trim($firstname)));
+}
+
+function cleanEmail($email){
+	return strtolower(trim($email));
+}
--- core/registerUser.php
+++ core/registerUser.php
@@ -1,5 +1,9 @@
 <?php
 
+require "functions.php";
+
+
+
 //Objectif : Insérer en BDD l'utilisateur (9 champs)
 
 //Récupérer les valeurs de l'internaute
@@ -35,24 +39,57 @@ if( count($_POST) != 9
 // -> Prénom
 // -> Nom
 // -> Email
+$gender = $_POST["gender"];
+$lastname = cleanLastname($_POST["lastname"]);
+$firstname = cleanFirstname($_POST["firstname"]);
+$email = cleanEmail($_POST["email"]);
+$pwd = $_POST["pwd"];
+$pwdConfirm = $_POST["pwdConfirm"];
+$city = $_POST["city"];
+$birthday = $_POST["birthday"];
 
+
+$listOfErrors = [];
 
 //Vérification micro des valeurs
+
 // Gender -> Soit 0, 1 ou 2
+$listOfGenders = [0,1,2];
+if( !in_array($gender, $listOfGenders) ){
+	$listOfErrors[] = "Le genre n'existe pas";
+}
 // Lastname -> >= 2 caractères
+if(strlen($lastname) < 2){
+	$listOfErrors[] = "Le nom doit faire plus de 2 caractères";
+}
 // Firstname -> >= 2 caractères
-// Date de naissance -> entre 6 et 80
+if(strlen($firstname) < 2){
+	$listOfErrors[] = "Le prénom doit faire plus de 2 caractères";
+}
 // City -> Soit 0, 1 ou 2
+$listOfCities = [0,1,2];
+if( !in_array($city, $listOfCities) ){
+	$listOfErrors[] = "La ville n'existe pas";
+}
 // Email -> Format
+if( !filter_var($email, FILTER_VALIDATE_EMAIL) ){
+	$listOfErrors[] = "L'email est incorrect";
+}
+
+
+
+// Date de naissance -> entre 6 et 80
 // Email -> Unicité
 // Pwd -> Min 8 caractères avec minuscules majuscules et chiffres
 //pwdConfirm -> = Pwd
 
 
-//SI OK
-//Insertion en BDD
-//Redirection login
-
-
-//SI NOK
-//Redirection sur register avec les erreurs
+if( empty($listOfErrors))
+{
+	//SI OK
+	//Insertion en BDD
+	//Redirection login
+}else{
+	//SI NOK
+	//Redirection sur register avec les erreurs
+}
